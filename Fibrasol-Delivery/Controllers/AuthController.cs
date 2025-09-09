18375e9f3c8ab036d310c6c24736588d116ae8ad
Luis Eduardo Mendoza <luisemendoza3@outlook.com>
2025-09-09 15:37:47 -0600

    Se agrego el Login controller

---
+++ Fibrasol-Delivery/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Fibrasol_Delivery.Request;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibrasol_Delivery.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    #region Views
+    [Route("login")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    #endregion
+
+
+    #region HTTP Methods
+    [HttpPost]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginRequest request)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+
+        if (user == null)
+            return Unauthorized();
+
+        var result = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, true, false);
+        if (!result.Succeeded)
+            return Unauthorized();
+
+        return Ok();
+    }
+    #endregion
+}
