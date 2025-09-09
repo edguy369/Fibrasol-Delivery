e6dea71c00b1f6914ca718ea0d0fc45cdff04308
Luis Eduardo Mendoza <luisemendoza3@outlook.com>
2025-09-09 15:56:19 -0600

    Se agrego la vista de Login

---
+++ Fibrasol-Delivery/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibrasol_Delivery.Controllers;
+
+public class HomeController : Controller
+{
+    [Authorize]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
