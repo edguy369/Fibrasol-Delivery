ddc4577c9fb43596ff2312aad53d2374ae3fb416
Luis Eduardo Mendoza <luisemendoza3@outlook.com>
2025-09-09 17:56:36 -0600

    Se implementaron los estados de las ordenes de envio

--- Fibrasol-Delivery/Controllers/DeliveryOrderStatusController.cs
+++ Fibrasol-Delivery/Controllers/DeliveryOrderStatusController.cs
@@ -1,4 +1,5 @@
 ﻿using Fibrasol_Delivery.Repository.Abstract;
+using Fibrasol_Delivery.Request;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fibrasol_Delivery.Controllers;
@@ -10,4 +11,55 @@ public class DeliveryOrderStatusController : Controller
     {
         _unitOfWork = unitOfWork;
     }
+
+    #region Views
+    [Route("estados-entrega")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    #endregion
+
+    #region Methods
+    [HttpGet]
+    [Route("delivery-statuses")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var deliveryOrderStatuses = await _unitOfWork.DeliveryOrderStatuses.GetAllAsync();
+        return Ok(deliveryOrderStatuses);
+    }
+
+    [HttpPost]
+    [Route("delivery-statuses")]
+    public async Task<IActionResult> CreateAsync([FromBody] DeliveryOrderStatusRequest request)
+    {
+        var transactionResult = await _unitOfWork.DeliveryOrderStatuses.CreateAsync(request);
+        if (transactionResult == 0)
+            return BadRequest();
+
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("delivery-statuses/{id}")]
+    public async Task<IActionResult> UpdateAsync(int id, [FromBody] DeliveryOrderStatusRequest request)
+    {
+        var transactionResult = await _unitOfWork.DeliveryOrderStatuses.UpdateAsync(id, request);
+        if (!transactionResult)
+            return BadRequest();
+
+        return Ok();
+    }
+
+    [HttpDelete]
+    [Route("delivery-statuses/{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var transactionResult = await _unitOfWork.DeliveryOrderStatuses.DeleteAsync(id);
+        if (!transactionResult)
+            return BadRequest();
+
+        return Ok();
+    }
+    #endregion
 }
