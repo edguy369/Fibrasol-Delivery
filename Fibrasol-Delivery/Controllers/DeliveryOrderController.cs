9bb26a2523c7ef0911c517b213d8a6363e401679
Luis Eduardo Mendoza <luisemendoza3@outlook.com>
2025-09-10 17:10:14 -0600

    Se creo la vista de ordenes de entrega apra agregar comandas

--- Fibrasol-Delivery/Controllers/DeliveryOrderController.cs
+++ Fibrasol-Delivery/Controllers/DeliveryOrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Fibrasol_Delivery.Repository.Abstract;
+using Fibrasol_Delivery.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +25,19 @@ public class DeliveryOrderController : Controller
     public IActionResult Detail(int id)
     {
         return View();
+    }
+    #endregion
+
+    #region HTTP Methods
+    [HttpPost]
+    [Route("delivery-orders")]
+    public async Task<IActionResult> CreateAsync([FromBody] DeliveryOrderRequest request)
+    {
+        var transactionResult = await _unitOfWork.DeliveryOrders.CreateAsync(request);
+        if (transactionResult == 0)
+            return BadRequest();
+
+        return Ok(transactionResult);
     }
     #endregion
 }
