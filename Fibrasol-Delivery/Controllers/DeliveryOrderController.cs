a849519e8455276886217fd9c6c8f19a813880f3
Luis Eduardo Mendoza <luisemendoza3@outlook.com>
2025-09-10 16:38:40 -0600

    Se implemento las vistas para crear Las ordenes de entrega

---
+++ Fibrasol-Delivery/Controllers/DeliveryOrderController.cs
@@ -0,0 +1,29 @@
+﻿using Fibrasol_Delivery.Repository.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibrasol_Delivery.Controllers;
+
+[Authorize]
+public class DeliveryOrderController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public DeliveryOrderController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    #region Views
+    [Route("constancias")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("constancias/{id}")]
+    public IActionResult Detail(int id)
+    {
+        return View();
+    }
+    #endregion
+}
